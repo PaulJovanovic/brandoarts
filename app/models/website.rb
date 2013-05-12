2f07d2b56a09b2660bfae416f63620ca917a01fa
Pavle Jovanovic <pavlej@vailsys.com>
2013-05-12 03:20:36 -0500

    last

--- app/models/website.rb
+++
@@ -1,7 +0,0 @@
-class Website < ActiveRecord::Base
-	default_scope order('placement ASC')
-	attr_accessible :photos_attributes, :description, :title, :photos, :url
-	has_many :photos, :as => :imageable
-	accepts_nested_attributes_for :photos, :allow_destroy => true
-	attr_accessor :placement
-end
