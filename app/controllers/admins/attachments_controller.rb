6eeb1c35c9a243e2828e61fded97f6a755e25cc1
Paul Jovanovic <paul@stacksocial.com>
2013-10-24 20:56:55 -0700

    Shit

--- app/controllers/admins/attachments_controller.rb
+++ app/controllers/admins/attachments_controller.rb
@@ -14,13 +14,22 @@ class Admins::AttachmentsController < ApplicationController
   end
 
   def create
-    @attachment = Attachment.new(params[:attachment], admin_id: current_admin.id)
+    @attachment = Attachment.new(params[:attachment].merge(admin_id: current_admin.id))
     if @attachment.save
       flash[:notice] = "Well done."
-      redirect_to admins_attachments_path
+      redirect_to admins_path
     else
       flash[:notice] = "Medium well."
       render new_admins_attachment_path
+    end
+  end
+
+  def update
+    @attachment = Attachment.where(admin_id: current_admin.id).last
+    if @attachment.update_attributes(params[:attachment])
+      redirect_to admins_path
+    else
+      render admins_attachment_path
     end
   end
 
